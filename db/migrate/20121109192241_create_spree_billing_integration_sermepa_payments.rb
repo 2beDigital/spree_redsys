85ae7f0366e9887f6b840fc37549b64b8379277a
picazoH <hector@ahaaa.es>
2013-02-20 22:18:53 +0100

    1.3 bumped

--- db/migrate/20121109192241_create_spree_billing_integration_sermepa_payments.rb
+++
@@ -1,8 +0,0 @@
-class CreateSpreeBillingIntegrationSermepaPayments < ActiveRecord::Migration
-  def change
-    create_table :spree_billing_integration_sermepa_payments do |t|
-
-      t.timestamps
-    end
-  end
-end
