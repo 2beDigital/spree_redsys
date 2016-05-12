23f709965de7397547c1aaf6e65e8cf2a0f05fe2
Pablo Torre <pablotorre81@gmail.com>
2016-05-12 10:47:33 +0200

    Require libraries: spree_backend & spree_frontend

--- lib/spree_redsys.rb
+++ lib/spree_redsys.rb
@@ -1,2 +1,8 @@
-require 'spree_core'
-require 'spree_redsys/engine'
+require "spree_core"
+require "spree_api"
+require "spree_backend"
+require "spree_frontend"
+
+
+require "spree_redsys/engine"
+require "spree_redsys/version"
