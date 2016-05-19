4c8c50c8bc6b5898cf8022c771b5b90d03434935
Pablo Torre <pablotorre81@gmail.com>
2016-05-19 13:43:50 +0200

    Refactoring code.

--- lib/spree_redsys.rb
+++ lib/spree_redsys.rb
@@ -6,3 +6,11 @@ require "spree_frontend"
 
 require "spree_redsys/engine"
 require "spree_redsys/version"
+
+module ActiveMerchant
+  module Billing
+    module Integrations
+      autoload :Redsys, 'active_merchant/billing/integrations/redsys'
+    end
+  end
+end
